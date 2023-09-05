6bf894f8f2c778e36df25d7a0f3f9b4df57a5355
Diogo Barrios <142221708+diogopxosys@users.noreply.github.com>
2023-09-05 15:44:04 +0100

    changed utils.py

--- cisco_support/utils.py
+++ cisco_support/utils.py
@@ -13,5 +13,15 @@ def getToken(client_id: str, client_secret: str, verify: bool, proxies: dict) ->
             'Content-Type':'application/x-www-form-urlencoded'
         }
 
-        r = requests.post(url=url, params=params, headers=headers, verify=verify, proxies=proxies)
-        return r.json()['access_token']
+        response = requests.post(url=url, params=params, headers=headers, verify=verify, proxies=proxies)
+        
+        if response is not None:
+           
+            try:
+               
+                return response.json()['access_token']
+            
+            except KeyError:
+                print("Error:", response.json()['error'],",Error Description:", response.json()['error_description'])
+        
+        return None
